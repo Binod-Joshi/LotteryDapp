ea0df12693b9c1fe85cc6afa129add50f25ed166
Binod-Joshi <111906023+Binod-Joshi@users.noreply.github.com>
2024-03-26 22:35:41 +0530

    smart contract started

---
+++ SmartContract/contracts/Lottery.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Lottery {
+   
+}
+
