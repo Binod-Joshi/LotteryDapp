9dc015d2218cf27560ef37ace0b41e7c8d3816af
Binod-Joshi <111906023+Binod-Joshi@users.noreply.github.com>
2024-03-28 10:49:48 +0530

    Lottery.sol completed

--- SmartContract/hardhat.config.js
+++ SmartContract/hardhat.config.js
@@ -3,4 +3,16 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations:1,
+    },
+    // sepolia: {
+    //   chainId: 11155111,
+    //   blockConfirmations: 6,
+
+    // }
+  }
 };
